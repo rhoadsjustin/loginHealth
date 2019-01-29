e5a0d184b16eb3ad52584feabb0b004a4bdb0017
Justin Rhoads <Justin.Rhodes@jbhunt.com>
2019-01-29 13:47:54 -0600

    stuff and things

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
 import React, { Component } from "react";
-import logo from "./mario_luigi_dance.gif";
+import logo from "./cryingLogo.png";
+import yes from './mario_luigi_dance.gif';
 import "./App.css";
-// import "primereact/resources/themes/nova-light/theme.css";
-// import "primereact/resources/primereact.min.css";
-// import "primeicons/primeicons.css";
-// import Chart from "./donutChart";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <div>
+          <img src={yes} className="App-logo" alt="logo" />
+          <div style={{ alignContent: 'center'}}>
             <h1 style={{ paddingTop: 20 }}>Is Login Down?</h1>
-            <h3 style={{ paddingTop: 20 }}>Nope!!!!!!!!!</h3>
+            <h3 style={{ paddingTop: 20 }}>Not. At. All.</h3>
           </div>
-          {/* <div>
-            <p>BUT KYLE's the man and fixed it, so login and keep working ya filthy animals.</p>
-            <img src={'https://ca.slack-edge.com/TCD5DF59Q-UCCU9933N-6a3683d6fe28-72'} className="kyle-logo" alt="logo" />
-
-          </div> */}
+          <div>
+            <p>Pi Basketball Scoreboard:</p>
+            <ul style={{ listStyle: 'none', alignItems: 'center' }}>
+              <li>Dane: 3</li>
+              <li>Justin: 2</li>
+              <li>Joe: 1</li>
+              <li>Brent: 0</li>
+            </ul>
+          </div>
         </header>
       </div>
     );
