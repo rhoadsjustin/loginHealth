466f5f3e639eaac604191c940fd84c4e7065f950
Justin Rhoads <justin.rhoads@jbhunt.com>
2019-01-31 14:21:52 -0600

    i win

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import logo from "./cryingLogo.png";
-import yes from './mario_luigi_dance.gif';
+import yes from "./mario_luigi_dance.gif";
 import "./App.css";
 
 class App extends Component {
@@ -9,15 +9,17 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={yes} className="App-logo" alt="logo" />
-          <div style={{ alignContent: 'center'}}>
+          <div style={{ alignContent: "center" }}>
             <h1 style={{ paddingTop: 20 }}>Is Login Down?</h1>
             <h3 style={{ paddingTop: 20 }}>Yup, JK. It's up.</h3>
           </div>
           <div>
             <p>Pi Basketball Scoreboard:</p>
-            <ul style={{ listStyle: 'none', alignItems: 'center' }}>
+            <ul style={{ listStyle: "none", alignItems: "center" }}>
+              <li>
+                Justin: 5 <img src={yes} style={{ height: 30, width: 30 }} />
+              </li>
               <li>Dane: 4</li>
-              <li>Justin: 3</li>
               <li>Joe: 1</li>
               <li>Brent: 0</li>
             </ul>
